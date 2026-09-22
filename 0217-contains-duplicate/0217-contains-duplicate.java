a9dc8582dd362f127e2af446265ba60a6a3050b7
Kaustubh Pandey <166645575+kaustub-1410@users.noreply.github.com>
2026-09-22 12:39:38 +0530

    Time: 19 ms (48.31%), Space: 111.1 MB (5.42%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,17 @@
+import java.util.HashMap;
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int num : nums) {
+            if (map.containsKey(num)) {
+                return true; // Duplicate found
+            }
+            map.put(num, 1);
+        }
+
+        return false; // No duplicates
+    }
+}
